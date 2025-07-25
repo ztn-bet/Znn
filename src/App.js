10e550f75033f36f3db45f23f671ceb1dd2bea20
ztn-bet <omarztn99@gmail.com>
2025-07-25 16:49:04 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
